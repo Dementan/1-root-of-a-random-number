f824894e0e98f0703f4fd3902351e84cb460532d
Dementan <masav7986@gmail.com>
2022-12-06 21:21:49 +0300

    Find the root of a random number [10,100] using math

---
+++ main.py
@@ -0,0 +1,5 @@
+# Find the root of a random number [10,100] using math
+
+import random,  math
+a = random.randint(10, 100)  # a = random integer [10,100] inclusive
+print(a, math.sqrt(a))  # sqrt - function of math
